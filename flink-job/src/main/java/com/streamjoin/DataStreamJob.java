b4f951c4f3fcd5b320ca6e406851244603708a30
Andrew-Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-08-04 16:01:15 -0500

    set logger to warn, ran smoke test

--- flink-job/src/main/java/com/streamjoin/DataStreamJob.java
+++ flink-job/src/main/java/com/streamjoin/DataStreamJob.java
@@ -20,18 +20,7 @@ package com.streamjoin;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+
 public class DataStreamJob {
 
 	public static void main(String[] args) throws Exception {
@@ -39,27 +28,12 @@ public class DataStreamJob {
 		// to building Flink applications.
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
-		 *
-		 * then, transform the resulting DataStream<Long> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide:
-		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
-		 *
-		 */
+		env.fromSequence(1,10)
+				.map(n -> "n=" + n)
+				.print();
+
 
 		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		env.execute("Flink Smoke Test");
 	}
 }
