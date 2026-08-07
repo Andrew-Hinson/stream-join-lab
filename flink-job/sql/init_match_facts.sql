5eb1a861cd894f21475a56f573dd356a77d41fd9
Andrew-Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-08-07 12:39:22 -0500

    fleshing out iceberg model

--- flink-job/sql/init_match_facts.sql
+++ flink-job/sql/init_match_facts.sql
@@ -10,15 +10,29 @@ CREATE DATABASE IF NOT EXISTS lake.demo;
 
 CREATE TABLE lake.demo.match_facts (
     match_id BIGINT,
-    player_id BIGINT,
     map_name STRING,
-    hero_played STRING,
+    match_duration_seconds INT,
+    started_at TIMESTAMP(3),
+    ended_at TIMESTAMP(3),
+    winning_team INT
+    
+    -- match_participants + players
+    player_id BIGINT,
+    account_name STRING,
     team INT,
+    hero_played STRING,
     kills INT,
     deaths INT,
+    healing INT,
     result STRING,
-    rank_tier STRING,
-    started_at TIMESTAMP(3)
+    
+    -- ranks frozen at match time
+    rank_tier_at_match_start STRING,
+    rank_division_at_match_start INT,
+    rank_points_at_match_start INT,
+
+    -- Flink emit time
+    ingested_at TIMESTAMP(3)
 ) WITH (
     'format-version' = '2',
     'write.format.default' = 'parquet'
