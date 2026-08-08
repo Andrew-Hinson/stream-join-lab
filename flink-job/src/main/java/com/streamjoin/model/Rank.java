c3d87724782510bfeea0c6b45a8e32ed8820c601
Andrew-Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-08-07 20:04:14 -0500

    finish java classes

---
+++ flink-job/src/main/java/com/streamjoin/model/Rank.java
@@ -0,0 +1,13 @@
+package com.streamjoin.model;
+
+import java.time.Instant;
+
+public class Rank {
+    public long playerId;
+    public String rankTier;
+    public int rankDivision;
+    public int rankPoints;
+    public int wins;
+    public int losses;
+    public Instant updatedAt;
+}
